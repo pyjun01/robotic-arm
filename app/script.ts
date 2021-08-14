0562a7757c0c1a24b0ba1d542b0f5adf9a6ff983
pyjun01 <pyjun02@gmail.com>
2021-08-15 01:57:56 +0900

    initial commit

---
+++ app/script.ts
@@ -0,0 +1,13 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = 500;
+const height = 500;
+
+canvas.width = width;
+canvas.height = height;
+
+const center = {
+  x: width / 2,
+  y: width / 2,
+};
