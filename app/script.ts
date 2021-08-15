597553db6c605d9e3028b20464b76e04e0d38d10
pyjun01 <pyjun02@gmail.com>
2021-08-15 23:00:26 +0900

    Render robotic arm

--- app/script.ts
+++ app/script.ts
@@ -11,3 +11,68 @@ const center = {
   x: width / 2,
   y: width / 2,
 };
+
+console.log(ctx);
+
+const armSize = {
+  width: width / 4,
+  height: 20,
+};
+
+const renderAxis = (x, y, size = 25) => {
+  ctx.beginPath();
+  ctx.fillStyle = '#b8c1c6';
+  ctx.strokeStyle = '#eee';
+  ctx.arc(x, y, size, 0, Math.PI * 2);
+  ctx.fill();
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.fillStyle = '#4c4648';
+  ctx.arc(x, y, size * 0.6, 0, Math.PI * 2);
+  ctx.fill();
+  ctx.closePath();
+}
+
+ctx.save();
+ctx.beginPath();
+ctx.strokeStyle = '#b8c1c6';
+ctx.lineWidth = armSize.height;
+ctx.lineCap = 'round';
+
+ctx.moveTo(center.x, center.y);
+ctx.lineTo(center.x - armSize.width, center.y);
+ctx.stroke();
+
+ctx.restore();
+
+ctx.save();
+ctx.beginPath();
+ctx.strokeStyle = '#b8c1c6';
+ctx.lineWidth = armSize.height;
+
+ctx.moveTo(center.x - armSize.width, center.y);
+ctx.lineTo(center.x - armSize.width * 2, center.y);
+ctx.stroke();
+
+ctx.restore();
+
+ctx.beginPath(); // 테두리
+ctx.fillStyle = '#b8c1c6';
+ctx.arc(center.x, center.y, width / 2, 0, Math.PI * 2);
+ctx.stroke();
+
+// 중앙
+ctx.beginPath();
+ctx.fillStyle = '#b8c1c6';
+ctx.arc(center.x, center.y, 25, 0, Math.PI * 2);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = '#4c4648';
+ctx.arc(center.x, center.y, 15, 0, Math.PI * 2);
+ctx.fill();
+
+renderAxis(center.x, center.y);
+
+renderAxis(center.x - armSize.width, center.y, 18);
